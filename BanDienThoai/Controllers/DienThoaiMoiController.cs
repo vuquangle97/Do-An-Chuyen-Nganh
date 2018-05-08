33e06d0bfc11de342535c37d65e4cdd264171f13
lequangvu151250533162 <vuquangle1997@gmail.com>
2018-05-09 02:51:18 +0700

    commit đầu tiên

---
+++ BanDienThoai/Controllers/DienThoaiMoiController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BanDienThoai.Models;
+namespace BanDienThoai.Controllers
+{
+    public class DienThoaiMoiController : Controller
+    {
+        // GET: DienThoaiMoi
+        WebsiteBanHangEntities db = new WebsiteBanHangEntities();
+        public PartialViewResult DienThoaiMoiPartial()
+        {
+            var listDienThoaiMoi = db.SanPhams.Take(3).ToList();
+            return PartialView(listDienThoaiMoi);
+        }
+    }
+}
---
+++ BanDienThoai/Controllers/NhaCungCapController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BanDienThoai.Models;
+
+namespace BanDienThoai.Controllers
+{
+    public class NhaCungCapController : Controller
+    {
+        WebsiteBanHangEntities db = new WebsiteBanHangEntities();
+        // GET: NhaCungCap
+        public ActionResult NhaCungCapPartial()
+        {
+            return PartialView(db.NhaCungCaps.Take(5).ToList());
+        }
+    }
+}
